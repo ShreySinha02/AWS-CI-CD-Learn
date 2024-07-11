1471910c5a6f7f78f2270dd6003bd67bfdcf273b
shreysinha25 <shrey.sinha@skyvoyage.in>
2024-07-11 16:17:55 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+ 
+      <div className=' bg-emerald-700 w-full h-screen  justify-center items-center flex'>
+
+        <h1 className=' text-rose-600 text-6xl m-auto  '>Hello Shrey</h1>
+      </div>
+   
+  )
+}
+
+export default App
